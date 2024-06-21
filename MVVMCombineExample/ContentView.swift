194e5cbb46b0ec259685960cdc3f246fa8cd163c
Safwen <safwen.debbichi-prestataire@bforbank.com>
2024-06-21 15:03:21 +0200

    Initial Commit

---
+++ MVVMCombineExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMCombineExample
+//
+//  Created by Safwen DEBBICHI on 21/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMCombineExample/MVVMCombineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCombineExampleApp.swift
+//  MVVMCombineExample
+//
+//  Created by Safwen DEBBICHI on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCombineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
